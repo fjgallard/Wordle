5b8f4de2a97b31ccc8505cea713ff965ac046c58
Francis Gallardo <fjdgallardo@gmail.com>
2022-04-29 18:45:18 +0800

    Removed board component.

--- src/app/modules/game/game.module.ts
+++ src/app/modules/game/game.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { KeyboardComponent } from './components/keyboard/keyboard.component';
-import { BoardComponent } from './components/board/board.component';
 import { AttemptComponent } from './components/attempt/attempt.component';
 import { GameComponent } from './pages/game/game.component';
 import { GameRoutingModule } from './game-routing.module';
@@ -11,7 +10,6 @@ import { GameRoutingModule } from './game-routing.module';
 @NgModule({
   declarations: [
     KeyboardComponent,
-    BoardComponent,
     AttemptComponent,
     GameComponent
   ],
@@ -20,8 +18,7 @@ import { GameRoutingModule } from './game-routing.module';
     GameRoutingModule
   ],
   exports: [
-    KeyboardComponent,
-    BoardComponent
+    KeyboardComponent
   ]
 })
 export class GameModule { }
